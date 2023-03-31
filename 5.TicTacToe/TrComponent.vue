827afc21be5d334b19c62c5dd485740286fb8d23
장윤경 <yunkyeong@dencomm.ai>
2023-03-31 19:03:26 +0900

    Feat: this.set 기능 이용해 인덱스를 통해서 변경하는 데이터의 값 재렌더링 되게 변경

---
+++ 5.TicTacToe/TrComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr>
+      <td-component v-for="(cellData, index) in rowData" :key="index" :cell-index="index" :cell-data="cellData"
+                    :row-index="rowIndex"></td-component>
+    </tr>
+  </template>
+  
+  <script>
+    import TdComponent from './TdComponent';
+  
+    export default {
+      components: {
+        TdComponent,
+      },
+      data() {
+        return {
+          parent: '내가 니 애비다',
+        };
+      },
+      props: {
+        rowData: Array,
+        rowIndex: Number,
+      },
+    };
+  </script>
+  
